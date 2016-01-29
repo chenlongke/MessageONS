7dcb5e5431489c8aaa698be6a34cb3a396258505
陈龙科 <chenlongke@loveapp.cn>
2016-01-29 15:37:18 +0800

    打包

--- src/com/send/ons/ProducerTest.java
+++ src/com/send/ons/ProducerTest.java
@@ -31,27 +31,22 @@ public class ProducerTest {
 		    producer.start();
 		    boolean isrun = true;
 		    while(isrun){
-		    	Message msg = new Message(//Message Topic
-			            "Test-FG",
-			            //Message Tag,
-			            //可理解为Gmail中的标签，对消息进行再归类，方便Consumer指定过滤条件在ONS服务器过滤       
-			            "TagA",
-			            //Message Body
-			            //任何二进制形式的数据，ONS不做任何干预，需要Producer与Consumer协商好一致的序列化和反序列化方式
-			            "Hello ONS".getBytes("utf-8"));
+		    	Message msg = new Message(
+			            	"Test-FG",//Message Topic
+			            	"TagA",//标签
+			            	"Hello ONS".getBytes("utf-8")//message body
+		    			);
 			    msg.setKey("ORDERID_100");
 			    SendResult sendResult = producer.send(msg);
-		        logger.info("发送成功=========>"+sendResult);
-		        
-		        String sdf=new SimpleDateFormat("yyyy-MM-dd-HH").format(new Date());
-		        if(sdf == "2015-01-29-14"){
+			    String sdf=new SimpleDateFormat("yyyy-MM-dd-HH").format(new Date());
+		        logger.info("发送成功=========>"+sendResult.getMessageId()+"现在的时间是:=======>"+sdf);
+		        if(sdf == "2016-01-29-16"){
 		        	isrun = false;
 		        	break;
 		        }
 		    }
 		} catch (IOException e) {
 			logger.info("发送失败 了。。。。。。。。。。。。");
-			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
 	}
